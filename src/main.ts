dfee8f2e28a022022d682469b6038dd736c5e41d
ngoctruongbui2003 <ngoctruongbuii2003@gmail.com>
2024-12-26 19:26:43 +0700

    fix mqtt

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Global Prefix
   app.setGlobalPrefix('api/v1', { exclude: [''] });
-  
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.MQTT,
+    options: {
+      url: 'mqtt://103.216.117.115:3001:1883',
+    },
+  });
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT || 3000);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
